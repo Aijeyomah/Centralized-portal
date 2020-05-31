7b8f31085f15b6cfd88226a7c9d56980e458187e
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2020-05-31 04:16:21 -0700

    new changes

--- client/src/Components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Button.css'
-
-export const FormButton = (props) => {
-    return(
-        <button disabled={props.disabled} className= {props.disabled ? 'disabled' : 'submitBtn'} type='Submit'>{props.text}</button>
-    );
-}
