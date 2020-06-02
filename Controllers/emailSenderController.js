33fb512108617e2bd3496855b707cbf9835b3cd4
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2020-06-02 03:20:47 -0700

    new features

---
+++ Controllers/emailSenderController.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
+const db = require('../database')
+const queries = require('../query')
+
+const emailsendercontroller = (email_address, subject, text) => {
+    const email = process.env.Email;
+    const password = process.env.Password
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: email,
+            pass: password
+        }
+    });
+    const mailOptions = {
+        from: 'Team Comet',
+        to: email_address,
+        subject: subject,
+        text: text
+    };
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+}
+module.exports = emailsendercontroller
