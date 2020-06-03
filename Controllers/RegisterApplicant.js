790194d59f1aa42b05a54eeefe927710e455f6f0
rotez-ayenzi <34565413+rotez-ayenzi@users.noreply.github.com>
2020-06-03 23:05:27 +0100

    dd

--- Controllers/RegisterApplicant.js
+++ Controllers/RegisterApplicant.js
@@ -334,7 +334,13 @@ exports.updateTestScores = async (req, res) => {
                 return res.status(400).json({
                     status: "failure",
                     code: 400,
-                    message: "your assessment status has not been updated"
+                    message: "you are yet to take the test"
+                })
+            } else {
+                return res.status(200).json({
+                    status: 'success',
+                    code: 200,
+                    message: "your test scores has been updated"
                 })
             }            
         } else {
