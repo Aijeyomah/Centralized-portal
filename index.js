a95b0d130fb264b391daeed1e7595c687db29494
rotez-ayenzi <34565413+rotez-ayenzi@users.noreply.github.com>
2020-06-04 13:47:41 +0100

    done

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const fileupload = require('express-fileupload')
 let app = express();
  let port = process.env.PORT || 8000;
 
+ 
  app.use(bodyParser.json({limit: '10mb', extended: true}))
  app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
  
