c0014c303802dc4be1c1f8d173025af0d2137195
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2020-06-07 04:46:48 -0700

    new

--- Controllers/RegisterApplicant.js
+++ Controllers/RegisterApplicant.js
@@ -323,10 +323,10 @@ exports.updateTestScores = async (req, res) => {
         text: queries.updateAssessmentStatusQuery,
         values: [status, email_address]
     };
-    const queryObject3 = {
-        text: queries.AdminUpdateAssessmentQuery,
-        values: [status, batch_id]
-    };
+    // const queryObject3 = {
+    //     text: queries.AdminUpdateAssessmentQuery,
+    //     values: [status, batch_id]
+    // };
     console.log(queryObject1)
     try {
         const { rowCount, rows } = await db.query(queryObject)
@@ -343,9 +343,7 @@ exports.updateTestScores = async (req, res) => {
                 const { rowCount } = await db.query(queryObject2)
                 console.log(queryObject2)
                 if (rowCount > 0) {
-                    const { rowCount } = await db.query(queryObject3)
-                    console.log(queryObject3)
-                    if (rowCount > 0) {
+                   
                     return res.status(200).json({
                         status: "success",
                         code: 200,
@@ -353,7 +351,7 @@ exports.updateTestScores = async (req, res) => {
                     })
                 } 
             }}
-        }
+        
     }
     catch (error) {
         console.log(error)
